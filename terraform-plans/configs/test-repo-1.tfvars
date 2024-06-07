bd5a0f00de847ea746b0957c562d31d58ebfbe20
Chi Wai Chan <chanchiwairay@gmail.com>
2024-06-07 09:49:32 +0800

    Remove terraform plan testing.

--- terraform-plans/configs/test-repo-1.tfvars
+++
@@ -1,8 +0,0 @@
-repository = "test-repo-1"
-branch     = "main"
-workflow_files = {
-  codeowners = {
-    source      = "./files/github/CODEOWNERS"
-    destination = ".github/CODEOWNERS"
-  }
-}
--- terraform-plans/imports.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = module.github_settings.github_repository.repo
-  id = var.repository
-}
-
-import {
-  to = module.github_settings.github_branch_protection.branch_protection
-  id = "${var.repository}:${var.branch}"
-}
